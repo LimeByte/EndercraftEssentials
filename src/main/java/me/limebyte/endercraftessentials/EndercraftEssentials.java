77f7083ae5f9c56d04e6a3c7790de23303b21eac
LimeByte <limebyte@twf.co.nz>
2012-08-12 16:32:45 +1200

    Added Logger.

--- src/main/java/me/limebyte/endercraftessentials/EndercraftEssentials.java
+++ src/main/java/me/limebyte/endercraftessentials/EndercraftEssentials.java
@@ -1,5 +1,7 @@
 package me.limebyte.endercraftessentials;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -13,15 +15,33 @@ public class EndercraftEssentials extends JavaPlugin {
 	private static EndercraftEssentials instance;
 	
 	/**
+	 * The CraftBukkit console logger for this JavaPlugin.
+	 */
+	private static Logger logger;
+	
+	/**
 	 * Cast when this JavaPlugin is enabled by the server instance.
 	 */
 	@Override
 	public final void onEnable() {
-		// Set instance
+		// Set instance variables
 		instance = this;
+		logger = this.getLogger();
 		
 		// Register Events
 		this.getServer().getPluginManager().registerEvents(new EventListener(), this);
+		
+		// Log enable message
+		this.log().info("Enabled!");
+	}
+	
+	/**
+	 * Cast when this JavaPlugin is disabled by the server instance.
+	 */
+	@Override
+	public final void onDisable() {
+		// Log disable message
+		this.log().info("Disabled.");
 	}
 	
 	/**
@@ -30,6 +50,14 @@ public class EndercraftEssentials extends JavaPlugin {
 	 */
 	public final EndercraftEssentials getInstance() {
 		return instance;
+	}
+	
+	/**
+	 * Gets the current console logger for EndercraftEssentials.
+	 * @return The current logger
+	 */
+	public final Logger log() {
+		return logger;
 	}
 	
 }
