ea7d409e0eb863698adcb701f72bc191b74425cd
LimeByte <limebyte@twf.co.nz>
2012-08-13 19:07:56 +1200

    Added a SplashScreen.

--- src/main/java/me/limebyte/endercraftessentials/EventListener.java
+++ src/main/java/me/limebyte/endercraftessentials/EventListener.java
@@ -1,10 +1,22 @@
 package me.limebyte.endercraftessentials;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.getspout.spoutapi.event.spout.SpoutCraftEnableEvent;
 
 /**
  * Represents the class which manages all events used by this JavaPlugin.
  */
 public class EventListener implements Listener {
 
+	/**
+	 * Fired when Spoutcraft is enabled by the client.
+	 * @param event The event fired
+	 */
+	@EventHandler
+	public final void onSpoutcraftEnable(final SpoutCraftEnableEvent event) {
+		SplashScreen splash = new SplashScreen(event.getPlayer());
+		splash.attach("http://cdn.spout.org/img/logo/spout_328x145.png");
+	}
+	
 }
