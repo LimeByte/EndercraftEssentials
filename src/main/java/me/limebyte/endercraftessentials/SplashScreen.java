b03a324f1a61a9b8a650dfd9bc5d5260ebb02faf
LimeByte <limebyte@twf.co.nz>
2012-08-14 14:10:25 +1200

    Animation time!

--- src/main/java/me/limebyte/endercraftessentials/SplashScreen.java
+++ src/main/java/me/limebyte/endercraftessentials/SplashScreen.java
@@ -1,7 +1,9 @@
 package me.limebyte.endercraftessentials;
 
 import org.getspout.spoutapi.gui.GenericTexture;
+import org.getspout.spoutapi.gui.InGameHUD;
 import org.getspout.spoutapi.gui.WidgetAnchor;
+import org.getspout.spoutapi.gui.WidgetAnim;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class SplashScreen {
@@ -10,6 +12,7 @@ public class SplashScreen {
 	private GenericTexture image;
 	private static final int WIDTH = 328;
 	private static final int HEIGHT = 125;
+	private static final short DURATION = 20;
 	
 	
 	public SplashScreen(final SpoutPlayer player) {
@@ -19,13 +22,18 @@ public class SplashScreen {
 	/**
 	 * Attaches the splash screen to the player.
 	 */
-	public void attach(final String splashURL) {
+	public final void attach(final String splashURL) {
+		InGameHUD screen = player.getMainScreen();
+		
 		image = new GenericTexture();
-		image.setAnchor(WidgetAnchor.CENTER_CENTER);
+		image.setAnchor(WidgetAnchor.CENTER_LEFT);
 		image.setWidth(WIDTH).setHeight(HEIGHT);
-		image.setX(-WIDTH / 2).setY(-HEIGHT / 2);
+		image.setX(-WIDTH).setY(-HEIGHT / 2);
 		image.setUrl(splashURL).setDrawAlphaChannel(true);
-		player.getMainScreen().attachWidget(EndercraftEssentials.getInstance(), image);
+		
+		screen.attachWidget(EndercraftEssentials.getInstance(), image);
+		screen.animate(WidgetAnim.POS_X, WIDTH / 2 + (screen.getWidth() / 2), DURATION, (short) 1, false, false);
+		screen.animateStart();
 	}
 	
 }
