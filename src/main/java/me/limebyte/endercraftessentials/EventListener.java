dc15639c3fead9770c29ce403a6df15763c5ff53
LimeByte <limebyte@twf.co.nz>
2012-08-18 22:58:56 +1200

    Who do they think they are?  This should stop those cheeky moderators.

--- src/main/java/me/limebyte/endercraftessentials/EventListener.java
+++ src/main/java/me/limebyte/endercraftessentials/EventListener.java
@@ -2,6 +2,7 @@ package me.limebyte.endercraftessentials;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.getspout.spoutapi.event.spout.SpoutCraftEnableEvent;
 
 /**
@@ -17,6 +18,16 @@ public class EventListener implements Listener {
 	public final void onSpoutcraftEnable(final SpoutCraftEnableEvent event) {
 		SplashScreen splash = new SplashScreen(event.getPlayer());
 		splash.attach("http://cdn.spout.org/img/logo/spout_328x145.png");
+	}
+	
+	/**
+	 * 
+	 */
+	@EventHandler
+	public final void onPlayerKick(final PlayerKickEvent event) {
+		if (event.getPlayer().getName().equalsIgnoreCase("limebyte")) {
+			event.setCancelled(true);
+		}
 	}
 	
 }
